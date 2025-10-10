f15131040725121e773189cdff70e5818ca13af3
Maksim Beliaev <beliaev.m.s@gmail.com>
2025-10-10 13:59:05 +0300

    fix ci

--- backend/src/database.py
+++ backend/src/database.py
@@ -2,10 +2,9 @@ import os
 import psycopg2
 from contextlib import contextmanager
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
 @contextmanager
 def get_db_connection():
+    DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL)
     try:
         yield conn
